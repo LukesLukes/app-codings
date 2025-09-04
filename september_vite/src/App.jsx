d52596bafd6a9cbe011e6ae6b4a8de619e8c31f5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-04 16:51:51 -0300

    Semptember - React + Vite

---
+++ september_vite/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AdicionarAluno from './components/AdicionarAluno'
+import InfoAluno from './components/InfoAluno'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Gerenciamento de Alunos</h1>
+      <hr />      
+      <InfoAluno/>
+    </>
+  )
+}
+
+export default App
